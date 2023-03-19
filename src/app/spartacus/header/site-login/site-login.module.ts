8488339f8558e84bf4109c245bb7436aee418473
Santosh Kori <santosh.kori@gmail.com>
2023-03-19 22:42:41 +0530

    Added siteLogin

---
+++ src/app/spartacus/header/site-login/site-login.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SiteLoginComponent } from './site-login.component';
+
+
+
+@NgModule({
+  declarations: [
+    SiteLoginComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SiteLoginComponent
+  ]
+})
+export class SiteLoginModule { }
